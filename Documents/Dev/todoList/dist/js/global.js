64606eb2e165bd42a9785096007182dd4d97a5b3
Yogev Lahyani <yogevlahyani@gmail.com>
2016-04-24 02:45:08 +0300

    First commit

---
+++ Documents/Dev/todoList/dist/js/global.js
@@ -0,0 +1,24 @@
+var app = angular.module("todoList", [
+
+]);
+
+app.controller("mainCtrl", ['$scope', function($scope) {
+	$scope.numbers = ['0','1','2','3','4','5','6','7','8','9','+','-','/','*'];
+
+	$scope.expression = '';
+	$scope.result = '';
+
+	$scope.add = function(x) {
+		$scope.expression += '' + x;
+	}
+
+	$scope.showResult = function() {
+		$scope.result = eval($scope.expression);
+	}
+
+	$scope.reset = function() {
+		$scope.expression = '';
+		$scope.result = '';
+	}
+
+}]);
