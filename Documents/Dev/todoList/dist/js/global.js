2b78d75aa77af6670d3003aabe804d32c5a56a6d
Yogev Lahyani <yogevlahyani@gmail.com>
2016-04-24 03:28:19 +0300

    First Commit

--- Documents/Dev/todoList/dist/js/global.js
+++ Documents/Dev/todoList/dist/js/global.js
@@ -3,7 +3,7 @@ var app = angular.module("todoList", [
 ]);
 
 app.controller("mainCtrl", ['$scope', function($scope) {
-	$scope.numbers = ['0','1','2','3','4','5','6','7','8','9','+','-','/','*'];
+	$scope.numbers = ['0','1','2','3','4','5','6','7','8','9','+','-','/','*','.'];
 
 	$scope.expression = '';
 	$scope.result = '';
@@ -19,6 +19,13 @@ app.controller("mainCtrl", ['$scope', function($scope) {
 	$scope.reset = function() {
 		$scope.expression = '';
 		$scope.result = '';
+	}
+
+	$scope.checkValidNumbers = function() {
+		if($scope.numbers.indexOf($scope.expression))
+			return false
+		else
+			return true
 	}
 
 }]);
